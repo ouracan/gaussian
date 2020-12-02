fe9101caa45db8809a0e38bbe8ff1ba215f652d8
Zheng Zu <zhengzu@pm.me>
2020-12-01 17:13:48 -0800

    add build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-g++  main.o function_implementation.o -o demo -lglfw3 -lGLU -Wno-write-strings  -framework OpenGL -framework GLUT -framework Cocoa -framework IOKit -framework CoreVideo                   
+emag++  main.o function_implementation.o -o demo -lglfw3 -lGLU -Wno-write-strings  -framework OpenGL -framework GLUT -framework Cocoa -framework IOKit -framework CoreVideo                   
 
